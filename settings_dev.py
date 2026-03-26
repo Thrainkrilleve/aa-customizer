0110fe9639f5d5b123e8607ac96103b51aa83e54
Thrain <austin.thompson1@proton.me>
2026-03-25 22:46:24 -0600

    Add CSRF middleware to settings_dev.py

--- settings_dev.py
+++ settings_dev.py
@@ -18,6 +18,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
 ]
 
 DATABASES = {
